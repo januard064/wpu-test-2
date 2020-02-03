d7c697ae5e5056cfc99f6f933efd0e00089da9ac
Januard064 <januargaol48@gmail.com>
2020-02-03 15:23:05 +0700

    menambah list di index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 <h1>Hello world</h1>
 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ex exercitationem iure expedita modi nisi iusto ad quasi, aperiam ea officia rem dolorem, nulla tempore a natus repellendus. Vel, sed.</h1>
-    
+
+<li>Tambah list</li>    
 </body>
 </html>
